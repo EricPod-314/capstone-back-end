ee48ea898c4fb503f8e6419b6f21b2d8c4f2b0a1
briggsy007 <hunter.briggs@outlook.com>
2021-07-30 15:27:49 -0400

    edit bucket

--- src/main/java/com/example/capstone/TestController.java
+++ src/main/java/com/example/capstone/TestController.java
@@ -1,11 +1,13 @@
 package com.example.capstone;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,14 @@ public class TestController {
     @GetMapping("/getByAccountId/{id}")
     public List<Bucket> getById(@PathVariable Long id) {
         return repo.findByAccountId(id);
+    }
+    @PutMapping("/editBucket/{id}")
+    public void editBucket(@PathVariable Long id, @RequestBody Map<String, String> Data){
+        Bucket bucket = repo.findById(id).get();
+        
+        Double percent = Double.parseDouble(Data.get("percent"));
+
+        bucket.setPercent(percent);
+        repo.save(bucket);
     }
 }
