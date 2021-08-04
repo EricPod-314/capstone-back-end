e883d8f425bf36086d01976a8869f1d80ccc5978
EricPod-314 <76055687+EricPod-314@users.noreply.github.com>
2021-08-04 11:54:54 -0400

    add and delete routes

--- src/main/java/com/example/capstone/TestController.java
+++ src/main/java/com/example/capstone/TestController.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -130,6 +131,15 @@ public class TestController {
             repo.save(i);
         }
     }
+
+    @PostMapping("/addNewBucket")
+    public void addNewBucket(@RequestBody Bucket bucket) {
+        repo.save(bucket);
+    }
     
+    @DeleteMapping("/deleteBucket")
+    public void deleteBucket(@RequestBody Bucket bucket) {
+        repo.deleteById(bucket.getId());
+    }
 
 }
